d6d11b8b1c34e1686be2892cc8f762afc7444823
PKCPH <patrick@klingaa.dk>
2024-05-23 13:50:39 +0200

    added asymmentric ecryption and deletes symmetric encryption (redundant for this project)

--- TodoBlazorApp/Handlers/EncryptionHandler.cs
+++ TodoBlazorApp/Handlers/EncryptionHandler.cs
@@ -2,35 +2,43 @@
 using System.Net.Http;
 using System.Security.Cryptography.X509Certificates;
 using System.Security.Cryptography;
+using System.Text;
 
 namespace TodoBlazorApp.Handlers;
 
 public class EncryptionHandler
 {
-
-    private readonly IDataProtector _dataProtector;
     private string _privateKey;
     private string _publicKey;
 
-
-    public EncryptionHandler(IDataProtectionProvider dataProtector)
+    public EncryptionHandler()
     {
-        _dataProtector = dataProtector.CreateProtector("NielsErVoresFavoritLærer");
+        if (!File.Exists("privateKey.pem"))
+        {
+            using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+            {
+                _privateKey = rsa.ToXmlString(true);
+                _publicKey = rsa.ToXmlString(false);
 
-        RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
-        _privateKey = rsa.ToXmlString(true);
-        _publicKey = rsa.ToXmlString(false);
-
+                File.WriteAllText("privateKey.pem", _privateKey);
+                File.WriteAllText("publicKey.pem", _publicKey);
+            }
+        }
+        else 
+        { 
+            _privateKey = File.ReadAllText("privateKey.pem"); 
+            _publicKey = File.ReadAllText("publicKey.pem");
+        }
 
     }
 
-    public static string AsymmetricEncryption(string textToEncrypt, string publicKey)
+    public string AsymmetricEncryption(string textToEncrypt)
     {
         using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
         {
-            rsa.FromXmlString(publicKey);
+            rsa.FromXmlString(_publicKey);
 
-            byte[] byteArrayTextToEncrypt = System.Text.Encoding.UTF8.GetBytes(textToEncrypt);
+            byte[] byteArrayTextToEncrypt = Encoding.UTF8.GetBytes(textToEncrypt);
             byte[] encryptedDataAsByteArray = rsa.Encrypt(byteArrayTextToEncrypt, true);
             var encryptedDataAsString = Convert.ToBase64String(encryptedDataAsByteArray);
 
@@ -46,7 +54,7 @@ public class EncryptionHandler
 
             byte[] byteArrayTextToDecrypt = Convert.FromBase64String(textToDecrypt);
             byte[] decryptedDataAsByteArray = rsa.Decrypt(byteArrayTextToDecrypt, true);
-            string decryptedDataAsString = System.Text.Encoding.UTF8.GetString(decryptedDataAsByteArray);
+            string decryptedDataAsString = Encoding.UTF8.GetString(decryptedDataAsByteArray);
 
             return decryptedDataAsString;
         }
